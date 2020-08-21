806cd6107430b00e326fe451714af93838a5f144
demon28 <274733956@qq.com>
2020-08-21 23:26:26 +0800

    添加项目文件。

---
+++ VueClass/wwwroot/js/MyImage.js
@@ -0,0 +1,18 @@
+﻿
+
+
+Vue.component("Myimage",{
+
+
+    props: {
+        imgname: "",
+        imgurl: ""
+
+    },
+    template: `<div style='text- align: center'>
+             <span> 图片名称：{{ imgname }} </span >
+            <br />
+         <img :src='imgurl'>
+        "</div>`
+
+});
